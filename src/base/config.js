b7a30a75a6f52a8c2d0d3f9a0264eb11de6b60f2
ars-abs <ars@abstractit.in>
2023-03-22 17:09:43 +0530

    Setup dotenv.

--- src/base/config.js
+++ src/base/config.js
@@ -1,14 +1,17 @@
 import { env } from 'process';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const {
 	NODE_ENV: nodeEnv = 'development',
 	// eslint-disable-next-line no-magic-numbers
 	PORT = 1235,
+	...rest
 } = env;
 
 const config = {
 	// #Note: ENV is destructured to save on access costs.
-	env: { ...env },
+	env: rest,
 	environment: nodeEnv,
 	port: PORT,
 };
