59662839e52654fcc88196a0c3252bc60164dc62
ars-abs <ars@abstractit.in>
2023-05-30 13:57:23 +0530

    Introduced some routes.

--- src/setup/setupRoutes.js
+++ src/setup/setupRoutes.js
@@ -1,9 +1,11 @@
 import info from '../controllers/info';
+import login from '../controllers/login';
 import ping from '../controllers/ping';
 
-const setupControllers = ({ app }) => {
+const setupRoutes = ({ app }) => {
 	app.get('/ping', ping);
 	app.get('/info', info);
+	app.get('/login', login);
 };
 
-export default setupControllers;
+export default setupRoutes;
