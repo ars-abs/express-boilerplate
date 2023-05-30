04f015ad89dfdb5cc3463375914b78b3c46333c9
ars-abs <ars@abstractit.in>
2023-05-30 14:10:43 +0530

    Introduce setupSteps.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import config from './base/config';
 import express from 'express';
 import setupServer from './setup/setupServer';
-import setupControllers from './setup/setupControllers';
+import setupRoutes from './setup/setupRoutes';
 import setupContext from './setup/setupContext';
+import { expressResources } from 'express-resources';
+import { expressAuth } from 'express-auth';
+import setupMiddleWares from './setup/setupMiddleWares';
+import setupProtectedRoutes from './setup/setupProtectedRoutes';
+import { map } from '@laufire/utils/collection';
 
 const main = () => {
 	const app = express();
 	const context = { app, config };
 
-	setupContext(context);
-	setupControllers(context);
-	setupServer(context);
+	const setupSteps = [
+		setupContext,
+		setupMiddleWares,
+		setupRoutes,
+		expressAuth,
+		setupProtectedRoutes,
+		expressResources,
+		setupServer,
+	];
+	const setup = (data) => map(setupSteps, (setupStep) => setupStep(data));
+
+	setup(context);
 };
 
 main();
