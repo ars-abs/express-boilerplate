d7d3a8d13107197808d08df2c968555e244958bd
dmdm-abs <dmdm@abstractit.in>
2023-06-19 11:38:05 +0530

    Introduced helpers/reduceSync.

---
+++ src/helpers/index.js
@@ -0,0 +1,25 @@
+import { keys } from '@laufire/utils/collection';
+
+const reduceSync = async (
+	collection, reducer, initial,
+) => {
+	let acc = initial;
+
+	const indexes = keys(collection);
+	const indexLength = indexes.length;
+
+	for(let i = 0; i < indexLength; i++) {
+		const index = indexes[i];
+
+		// eslint-disable-next-line no-await-in-loop
+		acc = await reducer(
+			acc, collection[index], index, collection,
+		);
+	}
+
+	return acc;
+};
+
+export {
+	reduceSync,
+};
