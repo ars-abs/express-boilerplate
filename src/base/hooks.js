8501b78dccd039c51c574ce768eb77ca58720fb0
dmdm-abs <dmdm@abstractit.in>
2023-08-16 17:27:24 +0530

    Removed authAction hook.

--- src/base/hooks.js
+++ src/base/hooks.js
@@ -1,25 +1,7 @@
 const fn = (req, res) => res.send('example hook');
 
-const authAction = async (req, res) => {
-	const { context: { repoCRUD }, params: { id: targetID }} = req;
-	const {
-		entity, action, payload: data, id,
-	} = await repoCRUD.signed.get({ id: targetID });
-
-	const props = {
-		get: { id },
-		getAll: { req },
-		create: { data },
-		update: { id, data },
-		remove: { id },
-	};
-
-	res.send(await repoCRUD[entity][action](props[action]));
-};
-
 const hooks = {
 	'example-hook': fn,
-	'auth-action/:id': authAction,
 };
 
 export default hooks;
