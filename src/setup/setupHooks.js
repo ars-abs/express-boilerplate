67d821a7f15f5d36823af7ee8b6be37a96719eed
dmdm-abs <dmdm@abstractit.in>
2023-08-16 17:30:41 +0530

    Refactored setupHooks.

--- src/setup/setupHooks.js
+++ src/setup/setupHooks.js
@@ -1,13 +1,13 @@
-import hooks from '@base/hooks';
+import customHooks from '@base/hooks';
 import { map } from '@laufire/utils/collection';
 import enrichReq from '@middlewares/enrichReq';
 
 const setupHooks = (context) => {
-	const { app } = context;
+	const { app, hooks = {}} = context;
 
 	app.use(enrichReq(context));
 
-	return map(hooks, (fn, path) => app.get(`/${ path }`, fn));
+	return map({ ...customHooks, ...hooks }, (fn, path) => app.get(`/${ path }`, fn));
 };
 
 export default setupHooks;
