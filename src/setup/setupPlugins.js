f0f58de94c6dc0e1f95965d3314418c43fbb05f9
dmdm-abs <dmdm@abstractit.in>
2023-08-22 13:14:43 +0530

    Added schema plugin in setupPlugins.

--- src/setup/setupPlugins.js
+++ src/setup/setupPlugins.js
@@ -2,11 +2,13 @@ import { pipe } from '../helpers';
 import { setup as expressAuth } from 'express-auth-plugin';
 import { setup as expressResources } from 'express-resources';
 import { setup as signedURL } from 'setup-signed-url';
+import { setup as schema } from 'express-resources-schema';
 
 const setupPlugins = (context) => pipe([
 	expressAuth,
 	expressResources,
 	signedURL,
+	schema,
 ], context);
 
 export default setupPlugins;
