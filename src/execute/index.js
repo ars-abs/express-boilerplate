b87a691d8fd8682b29ac70a8d472ea91b4f0e544
dmdm-abs <dmdm@abstractit.in>
2023-08-22 19:14:19 +0530

    Refactored execute.

--- src/execute/index.js
+++ src/execute/index.js
@@ -1,14 +1,10 @@
 import { pipe } from '../helpers';
 import setupMiddleWare from './setupMiddleWare';
-import setupRoutes from './setupRoutes';
-import setupHooks from './setupHooks';
 import execPlugin from './execPlugin';
 import setupServer from './setupServer';
 
 const execute = (context) => pipe([
 	setupMiddleWare,
-	setupRoutes,
-	setupHooks,
 	execPlugin,
 	setupServer,
 ], context);
