52c0582cf867b8f124e1dc1a1551ee59f02a3687
dmdm-abs <dmdm@abstractit.in>
2023-08-22 19:10:34 +0530

    Added info in setupPlugins.

--- src/build/setupPlugins.js
+++ src/build/setupPlugins.js
@@ -3,12 +3,14 @@ import { setup as expressAuth } from 'express-auth-plugin';
 import { setup as expressResources } from 'express-resources';
 import { setup as signedURL } from 'setup-signed-url';
 import { setup as schema } from 'express-resources-schema';
+import { setup as info } from 'health-check';
 
 const setupPlugins = (context) => pipe([
 	expressAuth,
 	expressResources,
 	signedURL,
 	schema,
+	info,
 ], context);
 
 export default setupPlugins;
