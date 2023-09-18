b8a49f42495d3c72b7a5b56a74de60e6fadf0a0e
ars-abs <ars@abstractit.in>
2023-09-18 20:05:14 +0530

    Refactored executeMiddleware.

--- src/build/initPlugins.js
+++ src/build/initPlugins.js
@@ -1,14 +1,24 @@
 import { merge } from '@laufire/utils/collection';
 import { reduceSync } from '../helpers';
 
+const defaultMiddleware = (
+	req, res, next
+) => next();
+
 const initPlugins = (context) => {
 	const { config: { plugins }} = context;
-	const init = async (acc, pluginName) => {
-		const { init: plugin } = await import(pluginName);
-		const { resources = {}, ...rest } = await plugin(acc);
+	const init = async (acc, pluginPath, name) => {
+		const { init: plugin } = await import(pluginPath);
+		const {
+			resources = {},
+			middleware = defaultMiddleware,
+			...rest
+		} = await plugin(acc);
 
 		return merge(
-			{}, acc, rest, { config: { resources }}
+			{}, acc, rest,
+			{ config: { resources }},
+			{ middlewares: { [name]: middleware }}
 		);
 	};
 
--- src/execute/executePlugin/executeMiddleware.js
+++ src/execute/executePlugin/executeMiddleware.js
@@ -1,8 +1,7 @@
 import { map } from '@laufire/utils/collection';
 
-const executeMiddleware = ({ app, middlewares }) => {
-	map(middlewares, (middleware) =>
-		map(middleware, (fn) => app.use(fn)));
+const executeMiddleware = ({ app, middlewares, config: { plugins }}) => {
+	map(plugins, (plugin, name) => app.use(middlewares[name]));
 };
 
 export default executeMiddleware;
