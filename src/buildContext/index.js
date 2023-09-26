a6d19bf4e671c2a3eb6944023bf4531592cb67ce
dmdm-abs <dmdm@abstractit.in>
2023-09-26 18:00:22 +0530

    Removed getRepos.

--- src/buildContext/index.js
+++ src/buildContext/index.js
@@ -1,21 +1,19 @@
 import express from 'express';
 import packageInfo from '../../package.json';
 import config from '@base/config';
-import getRepos from './getRepos';
 import service from './service';
 import constants from '@base/constants';
 import { merge } from '@laufire/utils/collection';
 
 const enrichContext = (context) => {
 	const app = express();
-	const repos = getRepos({ config });
 	const info = {
 		name: packageInfo.name,
 		description: packageInfo.description,
 		version: packageInfo.version,
 		lastStartTime: new Date(),
 	};
-	const defaultContext = { app, config, constants, repos, info, service } ;
+	const defaultContext = { app, config, constants, info, service } ;
 
 	return merge(
 		{}, defaultContext, context
