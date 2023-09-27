ae2d40a06accc8c7dd7a03e4b171e12d04b8da69
dmdm-abs <dmdm@abstractit.in>
2023-09-27 12:21:16 +0530

    Restructured buildContext to enrichContext.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'module-alias/register';
 import { pipe } from './helpers';
 import build from './build';
 import execute from './execute';
-import enrichContext from './buildContext';
+import enrichContext from './enrichContext';
 import extendResources from './extendResources';
 
 const main = (context) => pipe([
