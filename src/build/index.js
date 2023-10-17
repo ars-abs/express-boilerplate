c438d715f776628bb0fb2e9bd47265b4ba98ba19
dmdm-abs <dmdm@abstractit.in>
2023-10-17 10:36:51 +0530

    Restructured extendResources to build.

--- src/build/index.js
+++ src/build/index.js
@@ -1,9 +1,11 @@
 import initPlugins from './initPlugins';
 import { pipe } from '../helpers';
 import setupPlugins from './setupPlugins';
+import extendResources from './extendResources';
 
 const build = (context) => pipe([
 	initPlugins,
+	extendResources,
 	setupPlugins,
 ], context);
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import { pipe } from './helpers';
 import build from './build';
 import execute from './execute';
 import enrichContext from './enrichContext';
-import extendResources from './extendResources';
 
 const main = (context) => pipe([
 	enrichContext,
-	extendResources,
 	build,
 	execute,
 ], context);
