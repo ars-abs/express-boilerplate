4b0a7ee8cfc07a08d08589d83b1e623407339990
dmdm-abs <dmdm@abstractit.in>
2023-10-17 17:10:19 +0530

    Installed babel-plugin-module-resolver.

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,18 @@ module.exports = (api) => {
 	api.cache(false);
 	return {
 		plugins: [
-			['@babel/plugin-transform-runtime',
-				{
-					regenerator: true,
-				}],
+			['@babel/plugin-transform-runtime', { regenerator: true }],
+			['babel-plugin-module-resolver', {
+				alias: {
+					'@base': './src/base',
+					'@helpers': './src/helpers',
+					'@middlewares': './src/middlewares',
+					'@controllers': './src/controllers',
+					'@getRepos': './src/getRepos',
+					'@setup': './src/setup',
+				},
+			}],
 		],
-		presets: [
-			'@babel/preset-env',
-		],
+		presets: ['@babel/preset-env'],
 	};
 };
