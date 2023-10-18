7cb82efc7190e4bc3bdf78ec0023de97a96824c7
dmdm-abs <dmdm@abstractit.in>
2023-10-18 19:04:06 +0530

    Removed config from enrichContext.

--- src/enrichContext/index.js
+++ src/enrichContext/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import packageInfo from '../../package.json';
-import config from '@base/config';
 import constants from '@base/constants';
 import { merge } from '@laufire/utils/collection';
 import genRepos from './genRepos';
@@ -18,7 +17,7 @@ const enrichContext = (context) => {
 	const processRepos = genRepos(context);
 
 	const defaultContext = {
-		app, config, constants, repos, processRepos, info, service,
+		app, constants, repos, processRepos, info, service,
 	};
 
 	return merge(
